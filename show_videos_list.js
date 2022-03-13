68886ba9f7dbaca66a492d60ffcae393c09c1825
haganech <haganeja@gmail.com>
2022-03-13 19:50:33 -0400

    automatic reload function

--- show_videos_list.js
+++ show_videos_list.js
@@ -1,70 +1,154 @@
 javascript:(
-    function(){
-        let parseDateViewAndTimeFormat = false;
+    async function(){
+        const thumbnail_xpath = ".//*[@id='thumbnail']//img/@src";
         let sort_key = "Posted";
         let sort_order = "desc";
+        let filter_word = "";
         const iter_target_xpath = '//*[@id="contents"]//ytd-grid-video-renderer';
         let label_xpaths = {
+            "Thumbnail" : thumbnail_xpath,
             "Title" : './/a[@id="video-title"]/@title',
             "href" : './/a[@id="video-title"]/@href',
             "Time" : './/ytd-thumbnail-overlay-time-status-renderer/span',
             "Views" : './/*[@id="metadata-line"]/span[1]',
             "Posted" : './/*[@id="metadata-line"]/span[2]',
-            "Streamed" : './/*[@id="metadata-line"]/span[2]',
+            "Style" : './/*[@id="metadata-line"]/span[2]',
             "Closed Caption" : './/*[@id="video-badges"]//span[contains(text(), "CC") or contains(text(), "字幕")]',
         }
 
-        let retrieved_list = retrieve(iter_target_xpath, label_xpaths);
-        render(retrieved_list, label_xpaths, parseDateViewAndTimeFormat, sort_key, sort_order);
 
+        await prepare_page();
+        let retrieved_list = await retrieve(iter_target_xpath, label_xpaths);
+        await render(retrieved_list, label_xpaths, sort_key, sort_order, false, false, "");
 
-        function render(list, label_xpaths, _parseDateViewAndTimeFormat, sort_key, sort_order){
-            const body = document.getElementsByTagName("body")[0];
+        async function prepare_page(){
 
-            if (document.getElementById('temp_video_list') != null){
-                document.getElementById('temp_video_list').remove();
+            let previous_scrollHeight = 0;
+            for (let i=0; i<10000; i++){
+                const current_scrollHeight = document.documentElement.scrollHeight;
+                console.log("previous_scrollHeight : " + previous_scrollHeight + ", current_scrollHeight : " + current_scrollHeight);
+                console.log("kick? " + (previous_scrollHeight < current_scrollHeight));
+                if (previous_scrollHeight < current_scrollHeight || document.querySelector("#spinner") != null){
+                    previous_scrollHeight = previous_scrollHeight + Math.floor(document.documentElement.clientHeight / 2);
+                    window.scrollTo(0, previous_scrollHeight);
+                    await sleep(500);
+                }else{
+                    break;
+                }
             }
-    
-            let textarea = document.createElement('div');
-            textarea.setAttribute("id", "temp_video_list");
-            textarea.style.cssText  = `
-                z-index: 5000;
-                font-size: 12px;
-                position:absolute;
-                top:70px;
-                left:100px;
-                background-color: rgb(250,250,245);
-                border-radius:0.5em; 
-                opacity:95%;
-                border: solid 0.2em #888;
-                padding: 0.5em;
-                z-index: 5000;
-            `;
-    
-            let close_button = document.createElement('button');
-            close_button.onclick = function (event){
-                document.getElementById('temp_video_list').remove();
-            }
-            close_button.innerHTML = "Close"
-            textarea.appendChild(close_button);
             
-            if (_parseDateViewAndTimeFormat == false){
-                let parse_number_button = document.createElement('button');
-                parse_number_button.onclick = function (event){
-                    render(list, label_xpaths, true, sort_key, sort_order);
+            window.scrollTo(0, 0);
+        }
+
+        function sleep(time_msec){
+            return new Promise(resolve =>{
+                setTimeout(()=>{
+                    resolve()
+                }, time_msec);
+            });
+        }
+
+        function render(list, label_xpaths, sort_key, sort_order, _parseDateViewAndTimeFormat, _showThubmnail, _filter_text){
+            label_xpaths["Index"] = "";
+
+            if(_showThubmnail == true){
+                let tmp = {"Thumbnail" : thumbnail_xpath};
+                label_xpaths = Object.assign(tmp, label_xpaths);
+            }else if("Thumbnail" in label_xpaths){
+                delete label_xpaths['Thumbnail'];
+            }
+
+            let textarea = null;
+            if (document.getElementById('temp_video_list') == null){
+                textarea = document.createElement('div');
+                textarea.setAttribute("id", "temp_video_list");
+                textarea.style.cssText  = `
+                    font-size: 12px;
+                    position: absolute;
+                    top: 70px;
+                    left: 5%;
+                    max-width: 90%;
+                    height: 80%;
+                    overflow-y: scroll;
+                    background-color: rgb(250,250,245);
+                    border-radius:0.5em; 
+                    border: solid 0.2em #888;
+                    padding: 0.5em;
+                    z-index: 5000;
+                    opacity: 95%;
+                `;
+                const body = document.getElementsByTagName("body")[0];
+                body.appendChild(textarea);
+
+                /* Add Close button */
+                let close_button = document.createElement('button');
+                close_button.style.cssText = `margin-right: 0.25em;`;
+                close_button.onclick = function (event){
+                    document.getElementById('temp_video_list').remove();
                 }
-                parse_number_button.innerHTML = "Parse number and date"
+                close_button.innerHTML = "Close"
+                textarea.appendChild(close_button);
+                
+                /* Add Formatetd button */
+                let parse_number_button = document.createElement('button');
+                parse_number_button.setAttribute("id", "parse_number_button");
+                parse_number_button.style.cssText = `margin-right: 0.25em;`;
+                parse_number_button.onclick = function (event){
+                    render(list, label_xpaths, sort_key, sort_order, (_parseDateViewAndTimeFormat == false), _showThubmnail, _filter_text);
+                }
+                parse_number_button.innerHTML = (_parseDateViewAndTimeFormat ? "Raw text" : "Formatted");
                 textarea.appendChild(parse_number_button);
+                
+                /* Add showThumbnail button */
+                let show_thumbnail_button = document.createElement('button');
+                show_thumbnail_button.setAttribute("id", "show_thumbnail_button");
+                show_thumbnail_button.style.cssText = `margin-right: 0.25em;`;
+                show_thumbnail_button.onclick = function (event){
+                    render(list, label_xpaths, sort_key, sort_order, _parseDateViewAndTimeFormat, (_showThubmnail == false), _filter_text);
+                }
+                show_thumbnail_button.innerHTML = (_showThubmnail ? "Hide Thubmnail" : "Show Thubmnail");
+                textarea.appendChild(show_thumbnail_button);
+
+                /* Add Filter Textbox */
+                let filter_textbox = document.createElement('input');
+                if(document.getElementById("filter_textbox" != null)){
+                    filter_textbox = document.getElementById("filter_textbox" != null);
+                }else{
+                    filter_textbox.setAttribute("id", "filter_textbox");
+                    filter_textbox.type = 'text';
+                    filter_textbox.placeholder = 'Filter by text';
+                    filter_textbox.value = _filter_text;
+                    filter_textbox.style.cssText = `margin-right: 0.25em;`;
+                    filter_textbox.onkeyup = function (event){
+                        console.log(document.getElementById("filter_textbox").value);
+                        const text = document.getElementById("filter_textbox").value;
+                        render(list, label_xpaths, sort_key, sort_order, _parseDateViewAndTimeFormat, _showThubmnail, text);
+                    }
+                }
+                textarea.appendChild(filter_textbox);
+
             }else{
-                let parse_number_button = document.createElement('button');
+                textarea = document.getElementById("temp_video_list");
+
+                let parse_number_button = document.getElementById("parse_number_button");
                 parse_number_button.onclick = function (event){
-                    render(list, label_xpaths, false, sort_key, sort_order);
+                    console.log("clickec: parse_number_button");
+                    render(list, label_xpaths, sort_key, sort_order, (_parseDateViewAndTimeFormat == false), _showThubmnail, _filter_text);
                 }
-                parse_number_button.innerHTML = "Raw text"
-                textarea.appendChild(parse_number_button);
+                parse_number_button.innerHTML = (_parseDateViewAndTimeFormat ? "Raw text" : "Formatted");
+
+                let show_thumbnail_button = document.getElementById("show_thumbnail_button");
+                show_thumbnail_button.onclick = function (event){
+                    console.log("clickec: show_thumbnail_button");
+                    render(list, label_xpaths, sort_key, sort_order, _parseDateViewAndTimeFormat, (_showThubmnail == false), _filter_text);
+                }
+                show_thumbnail_button.innerHTML = (_showThubmnail ? "Hide Thubmnail" : "Show Thubmnail");
+
+                document.getElementById("temp_video_list-table").remove();
             }
     
             let table = document.createElement('table');
+            table.setAttribute("id", "temp_video_list-table");
             table.style.cssText  = `
                 border-collapse: collapse;
                 color: #111;
@@ -83,7 +167,7 @@ javascript:(
                 span_sort_asc.innerHTML = `▴`;
                 span_sort_asc.onclick = function(){
                     apply_sort(list, x, "asc");
-                    render(list, label_xpaths, _parseDateViewAndTimeFormat, x, "asc");
+                    render(list, label_xpaths, x, "asc", _parseDateViewAndTimeFormat, _showThubmnail, _filter_text);
                 }
                 th.appendChild(span_sort_asc);
 
@@ -95,7 +179,7 @@ javascript:(
                 span_sort_desc.innerHTML = `▾`;
                 span_sort_desc.onclick = function(){
                     apply_sort(list, x, "desc");
-                    render(list, label_xpaths, _parseDateViewAndTimeFormat, x, "desc");
+                    render(list, label_xpaths, x, "desc", _parseDateViewAndTimeFormat, _showThubmnail, _filter_text);
                 }
                 th.appendChild(span_sort_desc);
 
@@ -104,17 +188,23 @@ javascript:(
             table.appendChild(tr_header)
     
             for(const node of list){
+                if (_filter_text != "" && node["Title"].toUpperCase().includes(_filter_text.toUpperCase()) == false){
+                    continue;
+                }
+
                 let tr = document.createElement('tr');
                 for (const key in label_xpaths){
                     let value = "";
                     if (key === "href"){
                         value = `<a href="${node["url"]}" target="_blank">${node["url"]}</a>`;
-                    }else if(key == "Streamed"){
-                        if(node["Streamed"] == "LIVE"){
+                    }else if(key == "Style"){
+                        if(node["Style"] == "LIVE"){
                             value = "<b style='color: #E55;'>LIVE</b>";
                         }else{
                             value = node[key];
                         }
+                    }else if(key == "Thumbnail"){
+                        value = `<img style='width: 4em;' src='${node[key]}' />`;
                     }else{
                         if (_parseDateViewAndTimeFormat == true && ((key + "-sort") in node) ) {
                             value = node[key + "-sort"];
@@ -129,7 +219,7 @@ javascript:(
             }
 
             textarea.appendChild(table);
-            body.appendChild(textarea);
+
     
             /***  Adjusting look and feel by CSS  ***/
             // Table header
@@ -165,17 +255,21 @@ javascript:(
             let index = 0;
             for(const node of $$(iter_target_xpath)){
                 let row_map = {};
-                row_map["index-sort"] = index;
+                row_map["Index"] = index;
                 for (const key in label_xpaths){
                     let value = $(label_xpaths[key], node);
                     if (key === "href"){
                         // Get absolute path
                         row_map["url"] = (new URL($(label_xpaths[key], node), location.href)).href;
-                    }else if(key == "Streamed"){
+                    }else if(key == "Style"){
                         if($(label_xpaths["Views"], node).match(/(watching|視聴中)/)){
                             value = "<b style='color: #E55;'>LIVE</b>";
+                        }else if (value.match(/(Streamed|配信済み)/)){
+                            value = "Streamed";
+                        }else if (row_map["Title"].toUpperCase().includes("#SHORTS")){
+                            value = "#Shorts";
                         }else{
-                            value = (value.match(/(Streamed|配信済み)/))? "Yes" : "";
+                            value = "";
                         }
                     }else if(key == "Closed Caption"){
                         if(value != ''){
@@ -272,14 +366,14 @@ javascript:(
 
         function apply_sort(list, key, order='asc'){
             if (key == "Posted"){
-                key = "index";
+                // key = "index";
                 order = (order == "asc")? "desc": "asc";
             }
             if (list.length > 0 && (key + "-sort") in list[0]){
                 key = key + "-sort";
             }
 
-            if(key.includes("Time") || key.includes("Views") || key.includes("index")){
+            if(key.includes("Time") || key.includes("Views") || key.includes("Index")){
                 /* Number format sorting */
                 if(order == 'asc'){
                     list.sort((a, b) => { return Number(a[key]) - Number(b[key]) });
